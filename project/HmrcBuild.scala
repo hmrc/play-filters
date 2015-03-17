c9fd630b6f28159b3b40e76f10dfcb14c8a2a026
petrnejedly <pnejedly@equalexperts.com>
2015-03-17 17:06:38 +0000

    Release 1.0.0

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -25,7 +25,7 @@ object HmrcBuild extends Build {
   import PublishingSettings._
 
   val appName = "play-filters"
-  val appVersion = "1.0.0-SNAPSHOT"
+  val appVersion = "1.0.0"
 
   lazy val microservice = Project(appName, file("."))
     .settings(version := appVersion)
