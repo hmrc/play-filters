9739d3dec04ee35485796c5593534c6d21abb8a9
Howy Perrin <howyp@users.noreply.github.com>
2015-11-17 16:48:26 +0000

    Update HmrcBuild.scala

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -44,7 +44,7 @@ private object AppDependencies {
 
   val compile = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current % "provided",
-    "uk.gov.hmrc" %% "play-auditing" % "0.2.0"
+    "uk.gov.hmrc" %% "play-auditing" % "0.3.0"
   )
 
   trait TestDependencies {
